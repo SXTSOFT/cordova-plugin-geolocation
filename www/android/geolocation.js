d9740d76bcd922ee7f5a9cdb188a8320fd0b57d5
jiuyuong <z@ricent.com>
2018-03-09 15:18:12 +0800

    高德

--- www/android/geolocation.js
+++ www/android/geolocation.js
@@ -29,43 +29,47 @@ var pluginToNativeWatchMap = {};
 
 module.exports = {
     getCurrentPosition: function (success, error, args) {
-        var win = function () {
-            var geo = cordova.require('cordova/modulemapper').getOriginalSymbol(window, 'navigator.geolocation'); // eslint-disable-line no-undef
-            geo.getCurrentPosition(success, error, args);
-        };
-        var fail = function () {
-            if (error) {
-                error(new PositionError(PositionError.PERMISSION_DENIED, 'Illegal Access'));
-            }
-        };
-        exec(win, fail, 'Geolocation', 'getPermission', []);
+        // var win = function () {
+        //     var geo = cordova.require('cordova/modulemapper').getOriginalSymbol(window, 'navigator.geolocation'); // eslint-disable-line no-undef
+        //     geo.getCurrentPosition(success, error, args);
+        // };
+        // var fail = function () {
+        //     if (error) {
+        //         error(new PositionError(PositionError.PERMISSION_DENIED, 'Illegal Access'));
+        //     }
+        // };
+        // exec(win, fail, 'Geolocation', 'getPermission', []);
+        exec(success, error,'Geolocation','getCurrentPosition',[]);
     },
 
     watchPosition: function (success, error, args) {
         var pluginWatchId = utils.createUUID();
 
-        var win = function () {
-            var geo = cordova.require('cordova/modulemapper').getOriginalSymbol(window, 'navigator.geolocation'); // eslint-disable-line no-undef
-            pluginToNativeWatchMap[pluginWatchId] = geo.watchPosition(success, error, args);
-        };
+        // var win = function () {
+        //     var geo = cordova.require('cordova/modulemapper').getOriginalSymbol(window, 'navigator.geolocation'); // eslint-disable-line no-undef
+        //     pluginToNativeWatchMap[pluginWatchId] = geo.watchPosition(success, error, args);
+        // };
 
-        var fail = function () {
-            if (error) {
-                error(new PositionError(PositionError.PERMISSION_DENIED, 'Illegal Access'));
-            }
-        };
-        exec(win, fail, 'Geolocation', 'getPermission', []);
+        // var fail = function () {
+        //     if (error) {
+        //         error(new PositionError(PositionError.PERMISSION_DENIED, 'Illegal Access'));
+        //     }
+        // };
+        // exec(win, fail, 'Geolocation', 'getPermission', []);
+        exec(success, error,'Geolocation','watchPosition',[]);
 
         return pluginWatchId;
     },
 
     clearWatch: function (pluginWatchId) {
-        var win = function () {
-            var nativeWatchId = pluginToNativeWatchMap[pluginWatchId];
-            var geo = cordova.require('cordova/modulemapper').getOriginalSymbol(window, 'navigator.geolocation'); // eslint-disable-line no-undef
-            geo.clearWatch(nativeWatchId);
-        };
+        // var win = function () {
+        //     var nativeWatchId = pluginToNativeWatchMap[pluginWatchId];
+        //     var geo = cordova.require('cordova/modulemapper').getOriginalSymbol(window, 'navigator.geolocation'); // eslint-disable-line no-undef
+        //     geo.clearWatch(nativeWatchId);
+        // };
 
-        exec(win, null, 'Geolocation', 'getPermission', []);
+        exec(null, null, 'Geolocation', 'clearWatch', []);
     }
+
+    
 };
